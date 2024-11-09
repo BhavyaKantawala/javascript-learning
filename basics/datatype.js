ed65a3830754683ede3d2ab86a0d98b59ac889cf
Bhavya Kantawala <bhavyakantawala@gmail.com>
2024-11-09 09:57:49 +0530

    Datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log( 3 + 2 ) // code readabitlity should be better
+
+console.log("Bhavya")
+
+let name = "Bhavya"
+let age = 17
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
